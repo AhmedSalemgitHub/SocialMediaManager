c61cd875a53dd9f21f3db670803b56a430c29543
Ahmed Salem <AhmedSalemgitHub@users.noreply.github.com>
2019-01-16 21:48:40 +0200

    add base64 Key

--- app/src/main/java/com/codingglobal/socialmediamanager/PostingScreen.java
+++ app/src/main/java/com/codingglobal/socialmediamanager/PostingScreen.java
@@ -1,10 +1,18 @@
 package com.codingglobal.socialmediamanager;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class PostingScreen extends AppCompatActivity {
 
@@ -22,13 +30,26 @@ public class PostingScreen extends AppCompatActivity {
         button_Post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PostingToSocialMedia();
+                getBase64Key();
             }
         });
 
     }
 
-    private void PostingToSocialMedia() {
+    private void getBase64Key() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.codingglobal.socialmediamanager"
+                    , PackageManager.GET_SIGNATURES);
 
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEY", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
